4b4f4cdcd9c23c0c06d47844965bc939ac06bb06
Mitchell Hodzen <MitchellHodzen@yahoo.com>
2017-08-03 16:08:56 -0400

    Texture now drawn on screen

--- OpenGLTesting/src/Renderer.cpp
+++ OpenGLTesting/src/Renderer.cpp
@@ -51,7 +51,7 @@ bool Renderer::Initialize()
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-		sdlWindow = SDL_CreateWindow("Falling Sand Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+		sdlWindow = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 		if (sdlWindow == NULL)
 		{
 			std::cout << "SDL Window could not be created. SDL error: " << SDL_GetError() << std::endl;
@@ -106,10 +106,11 @@ bool Renderer::InitOpenGL()
 	}
 	
 	float verticies[] = {
-		0.5f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f
+		//Position          Texture coords
+		0.5f, 0.5f, 0.0f,   1.0f, 1.0f,
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f,  0.0f, 1.0f
 	};
 	unsigned int indicies[] = {
 		0, 1, 3,
@@ -123,8 +124,10 @@ bool Renderer::InitOpenGL()
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GL_FLOAT), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)(3*sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
--- OpenGLTesting/src/shaders/fragmentShader1.frag
+++ OpenGLTesting/src/shaders/fragmentShader1.frag
@@ -1,6 +1,9 @@
 #version 450 core
 out vec4 FragColor;
+
+in vec2 texCoord;
+uniform sampler2D currentTexture;
 void main()
 {
-	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	FragColor = texture(currentTexture, texCoord);
 }
