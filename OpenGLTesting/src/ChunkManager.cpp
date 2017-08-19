01a1a5f7f922dcf6e7ab7ef8668f683491bc512c
Mitchell Hodzen <MitchellHodzen@yahoo.com>
2017-08-19 00:16:31 -0400

    Fixed bug causing the chunk manager to not take into account individual block sizes when querying chunks

--- OpenGLTesting/src/ChunkManager.cpp
+++ OpenGLTesting/src/ChunkManager.cpp
@@ -16,23 +16,19 @@ ChunkManager::~ChunkManager()
 std::vector<Chunk*> ChunkManager::QueryChunks(glm::vec3 playerPosition)
 {
 	//We add an offset of one half the block size because a block's origin is its center. May be easier to change blocks origin to corner
-	int chunkX = floor((playerPosition.x + world->GetBlockSize()/2) / chunkWidth);
-	int chunkY = floor((playerPosition.y + world->GetBlockSize()/2) / chunkHeight);
-	int chunkZ = floor((playerPosition.z + world->GetBlockSize()/2) / chunkLength);
+	int chunkX = floor((playerPosition.x + world->GetBlockSize()/2) / world->GetBlockSize() / chunkWidth);
+	int chunkY = floor((playerPosition.y + world->GetBlockSize()/2) / world->GetBlockSize() / chunkHeight);
+	int chunkZ = floor((playerPosition.z + world->GetBlockSize()/2) / world->GetBlockSize() / chunkLength);
 	//std::cout<<chunkX << ", " << chunkY << ", " << chunkZ << std::endl;
 	std::vector<Chunk*> chunks;
-	chunks.push_back(world->GetChunkAtPosition(chunkX, chunkY - 1, chunkZ));
-	chunks.push_back(world->GetChunkAtPosition(chunkX + 1, chunkY - 1, chunkZ));
-	chunks.push_back(world->GetChunkAtPosition(chunkX - 1, chunkY - 1, chunkZ));
-	chunks.push_back(world->GetChunkAtPosition(chunkX, chunkY - 1, chunkZ + 1));
-	chunks.push_back(world->GetChunkAtPosition(chunkX, chunkY - 1, chunkZ - 1));
-	/*
-	chunks.push_back(world->GetChunkAtPosition(0, -1, 0));
-	chunks.push_back(world->GetChunkAtPosition(1, -1, 0));
-	chunks.push_back(world->GetChunkAtPosition(-1, -1, 0));
-	chunks.push_back(world->GetChunkAtPosition(0, -1, -1));
-	chunks.push_back(world->GetChunkAtPosition(0, -1, 1));
-	*/
+	int amount = 1;
+	for(int x = chunkX - amount; x <= chunkX + amount; ++x)
+	{
+		for(int z = chunkZ - amount; z <= chunkZ + amount; ++z)
+		{
+			chunks.push_back(world->GetChunkAtPosition(x, chunkY - 1, z));
+		}
+	}
 	return chunks;
 }
 
